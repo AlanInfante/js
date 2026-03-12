d4d486db4e756ee00f52e6561d460bb2d0ecb037
alan <infantelobo@gmail.com>
2026-03-12 17:07:30 -0300

    jvEj1

--- main.js
+++ main.js
@@ -1,2 +1,36 @@
-alert ("HolaMundo");
-alert ("Gimnasia");
+let num1=22;
+let num2="22";
+/*if (num1===num2)
+{
+    alert ("Los numeros son iguales!");
+}
+else 
+{
+    alert ("Los numeros son distintos!");
+}
+*/
+//and &&
+//or  ||
+//not !
+let edad = Number (prompt ("Ingrese su edad!"));
+/*if (edad>=18)
+{
+    alert ("Felicitaciones fue aceptado!");
+}
+else
+{
+    alert ("Usted debe ser mayor a 18 años!");
+}
+*/
+if (edad<18 && edad > 0)
+{
+     alert ("Usted debe ser mayor a 18 años!");
+}
+else if (edad>=18 && edad<99)
+{
+    alert ("Felicitaciones fue aceptado!");
+}
+else
+{
+    alert ("Me estas boludeando");
+}
